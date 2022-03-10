4c3dccb5c4a5b6f95f105783ef0c189369b03222
khanshorif331 <khanshorif331@gmail.com>
2022-03-10 21:36:25 +0600

    comment is now showing.undefined bug fixed

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post.description);
+  // console.log(post.comments);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -121,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0].text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
